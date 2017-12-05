0646d9436c6a34bb8003be7326490149a98fcfaa
Tobias Florek <tob@butter.sh>
2017-12-05 11:08:41 +0100

    don't use error-chain

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-#[macro_use]
-extern crate error_chain;
 extern crate systemd;
 use systemd::journal::{Journal, JournalFiles, JournalSeek, JournalRecord};
-
-mod errors {
-    error_chain!{}
-}
-use errors::*;
+use systemd::Result;
 
 pub fn open_journal() -> Result<Journal> {
     let mut j =
-        Journal::open(JournalFiles::All, false, false).chain_err(|| "unable to open journal")?;
-    j.seek(JournalSeek::Tail).chain_err(|| "cannot seek to head")?;
+        Journal::open(JournalFiles::All, false, false)?;
+    j.seek(JournalSeek::Tail)?;
     Ok(j)
 }
 
 fn run() -> Result<()> {
     let mut j = open_journal()?;
-    j.watch_all_elements(f).chain_err(|| "unable to watch all elements")?;
+    j.watch_all_elements(f)?;
     Ok(())
 }
 
-pub fn f(rec: JournalRecord) -> systemd::Result<()> {
+pub fn f(rec: JournalRecord) -> Result<()> {
     for (key, value) in rec.iter() {
         println!("{}: {}", key, value)
     }
@@ -35,16 +29,6 @@ fn main() {
         let errmsg = "Error writing to stderr";
 
         writeln!(stderr, "error: {}", e).expect(errmsg);
-
-        for e in e.iter().skip(1) {
-            writeln!(stderr, "caused by: {}", e).expect(errmsg);
-        }
-
-        // The backtrace is not always generated. Try to run this example
-        // with `RUST_BACKTRACE=1`.
-        if let Some(backtrace) = e.backtrace() {
-            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
-        }
 
         ::std::process::exit(1);
     }
